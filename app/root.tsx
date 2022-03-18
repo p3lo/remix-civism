134c959e068f11a3e5d7bac1ac03dd323d2364b3
Daniel Slanicka <mysiacik@mysiacik.com>
2022-03-18 21:55:22 +0100

    initial commit

---
+++ app/root.tsx
@@ -0,0 +1,135 @@
+import { Link, Links, LinksFunction, LiveReload, Meta, Outlet, Scripts, ScrollRestoration, useCatch } from 'remix';
+import type { MetaFunction } from 'remix';
+import styles from './tailwind.css';
+import { useState } from 'react';
+import { MantineProvider, ColorSchemeProvider, ColorScheme, ActionIcon, useMantineColorScheme } from '@mantine/core';
+import { BsSun, BsMoon } from 'react-icons/bs';
+
+export const meta: MetaFunction = () => ({
+  charset: 'utf-8',
+  title: 'New Remix App',
+  viewport: 'width=device-width,initial-scale=1',
+});
+
+export const links: LinksFunction = () => [{ rel: 'stylesheet', href: styles }];
+
+export default function App() {
+  const [colorScheme, setColorScheme] = useState<ColorScheme>('light');
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+  return (
+    <Document>
+      <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+        <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
+          <Layout>
+            <Outlet />
+          </Layout>
+        </MantineProvider>
+      </ColorSchemeProvider>
+    </Document>
+  );
+}
+
+function Document({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        {children}
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
+
+function Layout({ children }: React.PropsWithChildren<{}>) {
+  const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+  const dark = colorScheme === 'dark';
+  return (
+    <div className="flex flex-col justify-between h-screen">
+      <header className="p-4 shadow-xl ">
+        <div className="flex justify-end ">
+          <ActionIcon
+            variant="outline"
+            color={dark ? 'yellow' : 'blue'}
+            onClick={() => toggleColorScheme()}
+            title="Toggle color scheme"
+          >
+            {dark ? <BsSun size={18} className="text-yellow-400" /> : <BsMoon size={18} className="text-blue-500" />}
+          </ActionIcon>
+        </div>
+      </header>
+      <main className="flex-grow">
+        <div className="">{children}</div>
+      </main>
+      <footer className="p-4 border-t"></footer>
+    </div>
+  );
+}
+
+export function ErrorBoundary({ error }: { error: Error }) {
+  console.error(error);
+  return (
+    <Document>
+      <div className="bg-white min-h-full px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
+        <div className="max-w-max mx-auto">
+          <main className="sm:flex">
+            <p className="text-4xl font-extrabold text-indigo-600 sm:text-5xl">500</p>
+            <div className="sm:ml-6">
+              <div className="sm:border-l sm:border-gray-200 sm:pl-6">
+                <h1 className="text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">
+                  Something went wrong!
+                </h1>
+                <p className="mt-1 text-base text-gray-500">Please check the URL in the address bar and try again.</p>
+              </div>
+              <div className="mt-10 flex space-x-3 sm:border-l sm:border-transparent sm:pl-6">
+                <Link
+                  to="/"
+                  className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  Go back home
+                </Link>
+              </div>
+            </div>
+          </main>
+        </div>
+      </div>
+    </Document>
+  );
+}
+
+export function CatchBoundary() {
+  let caught = useCatch();
+  return (
+    <Document>
+      <div className="bg-white min-h-full px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
+        <div className="max-w-max mx-auto">
+          <main className="sm:flex">
+            <p className="text-4xl font-extrabold text-indigo-600 sm:text-5xl">{caught.status}</p>
+            <div className="sm:ml-6">
+              <div className="sm:border-l sm:border-gray-200 sm:pl-6">
+                <h1 className="text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">
+                  {caught.statusText}
+                </h1>
+                <p className="mt-1 text-base text-gray-500">Please check the URL in the address bar and try again.</p>
+              </div>
+              <div className="mt-10 flex space-x-3 sm:border-l sm:border-transparent sm:pl-6">
+                <Link
+                  to="/"
+                  className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  Go back home
+                </Link>
+              </div>
+            </div>
+          </main>
+        </div>
+      </div>
+    </Document>
+  );
+}
