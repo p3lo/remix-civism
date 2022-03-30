7f809552787db6dc825cd0f73f6c869666d5fcfb
p3lo <mysiacik@mysiacik.com>
2022-03-30 14:32:53 +0200

    completed voting

--- app/components/PollItem.tsx
+++ app/components/PollItem.tsx
@@ -22,34 +22,37 @@ function PollItem({ poll }: { poll: Poll }) {
       spacing={5}
       styles={{ body: { pointerEvents: 'none' } }}
       target={
-        <div className="flex items-start border-b border-opacity-50 border-dotted">
-          <Link to={poll.slug} className="flex flex-col p-2 rounded-lg cursor-pointer grow group">
-            <p className="font-bold transition duration-150 ease-out transform group-hover:font-extrabold">
-              {poll.poll}
-            </p>
-            {poll.poll_description && <p className="pb-2 text-xs opacity-60">({poll.poll_description})</p>}
-            <div className="flex space-x-1 text-xs opacity-50">
-              <p>{poll.author.name}</p>
-              <div className="border-l" />
-              <p>{getDate(poll.created_at)}</p>
-            </div>
-          </Link>
-          <AiOutlineEye
-            className="w-5 h-5 m-2 transition duration-150 ease-out transform cursor-help hover:scale-125"
-            onMouseEnter={() => setOpened(true)}
-            onMouseLeave={() => setOpened(false)}
-          />
-        </div>
+        <>
+          <div className="flex items-start">
+            <Link to={poll.slug} className="flex flex-col p-2 rounded-lg cursor-pointer grow group">
+              <p className="font-bold transition duration-150 ease-out transform group-hover:font-extrabold">
+                {poll.poll}
+              </p>
+              {poll.poll_description && <p className="pb-2 text-xs opacity-60">({poll.poll_description})</p>}
+              <div className="flex space-x-1 text-xs opacity-50">
+                <p>{poll.author.name}</p>
+                <div className="border-l" />
+                <p>{getDate(poll.created_at)}</p>
+              </div>
+            </Link>
+            <AiOutlineEye
+              className="w-5 h-5 m-2 transition duration-150 ease-out transform cursor-help hover:scale-125"
+              onMouseEnter={() => setOpened(true)}
+              onMouseLeave={() => setOpened(false)}
+            />
+          </div>
+          <div className="w-full border-b opacity-20 border-dotted" />
+        </>
       }
     >
-      <div className="flex flex-col space-y-2 w-[450px]">
+      <div className="flex flex-col space-y-2 w-[450px] mb-3">
         <p className="mx-auto mt-2 font-bold">Poll votes</p>
         {poll.options.map((item) => (
           <div className="flex flex-col" key={item.id}>
-            <p className="text-sm">{item.option}</p>
+            <p className="text-sm truncate w-1/2">{item.option}</p>
             <div className="flex items-center space-x-1">
               <p className="text-xs opacity-50">{`${getPercent(item.votes, totalVotes)}%`}</p>
-              <Progress value={getPercent(item.votes, totalVotes)} size={25} radius="xs" className="grow" />
+              <Progress value={getPercent(item.votes, totalVotes)} size={18} radius="xs" className="grow" />
               <p className="text-xs opacity-50 w-[45px] text-center">({item.votes})</p>
             </div>
           </div>
--- app/routes/$slug.tsx
+++ app/routes/$slug.tsx
@@ -1,8 +1,10 @@
-import { ListItem, Progress, Radio, RadioGroup } from '@mantine/core';
-import { ActionFunction, Form, LoaderFunction, useLoaderData, useSubmit, useTransition } from 'remix';
+import { Progress, Radio, RadioGroup } from '@mantine/core';
+import { useLocalStorage } from '@mantine/hooks';
+import { useEffect } from 'react';
+import { ActionFunction, Form, LoaderFunction, useActionData, useLoaderData, useSubmit, useTransition } from 'remix';
 import { prisma } from '~/db.server';
 import { getDate, getPercent, sumVotes } from '~/utils/functions';
-import { Poll } from '~/utils/types';
+import { Option, Poll } from '~/utils/types';
 
 export const loader: LoaderFunction = async ({ params }) => {
   let slug = params.slug;
@@ -33,8 +35,23 @@ export const loader: LoaderFunction = async ({ params }) => {
 export const action: ActionFunction = async ({ request }) => {
   const formData = await request.formData();
   const option = formData.get('option');
-  if (option) {
+  const oldOption = formData.get('oldOption');
+
+  if (oldOption && +oldOption > 0) {
     await prisma.option.update({
+      where: {
+        id: +oldOption,
+      },
+      data: {
+        votes: {
+          decrement: 1,
+        },
+      },
+    });
+  }
+  let dbData;
+  if (option) {
+    dbData = await prisma.option.update({
       where: {
         id: +option,
       },
@@ -45,14 +62,22 @@ export const action: ActionFunction = async ({ request }) => {
       },
     });
   }
-
-  return null;
+  return dbData;
 };
 
 function PollSlug() {
   const poll: Poll = useLoaderData();
+  const getData: Option | undefined = useActionData();
   const submit = useSubmit();
   const transition = useTransition();
+  const [optionId, setOptionId] = useLocalStorage({ key: poll.id.toString(), defaultValue: 0 });
+
+  useEffect(() => {
+    if (getData) {
+      setOptionId(getData.id);
+    }
+  }, [getData]);
+
   function handleChange(event: React.FormEvent<HTMLFormElement>) {
     submit(event.currentTarget, { replace: true });
   }
@@ -61,7 +86,7 @@ function PollSlug() {
     <>
       <div className="w-full mx-auto my-10 sm:w-[80%] md:w-[65%] xl:w-[50%]">
         <p className="text-2xl font-bold text-center">{poll.poll}</p>
-        <p className="text-sm font-bold text-center opacity-50">({poll.poll_description})</p>
+        {poll.poll_description && <p className="text-sm font-bold text-center opacity-50">({poll.poll_description})</p>}
       </div>
       <Form method="post" onChange={handleChange}>
         <RadioGroup
@@ -69,15 +94,18 @@ function PollSlug() {
           className="w-full my-10 mx-auto sm:w-[80%] md:w-[65%] xl:w-[50%]"
           orientation="vertical"
           label="Select your preferable option"
+          {...(optionId > 0 && { defaultValue: `${optionId.toString()}` })}
         >
           {poll.options.map((item) => (
             // @ts-ignore
             <Radio key={item.id} value={item.id.toString()} label={item.option} disabled={transition.submission} />
           ))}
         </RadioGroup>
+        <input hidden name="oldOption" value={optionId} readOnly />
       </Form>
       <div className="w-full my-10 mx-auto sm:w-[80%] md:w-[65%] xl:w-[50%]">
         <p className="text-center mt-2 font-semibold text-sm">Results</p>
+        <p className="text-center text-xs opacity-50">(Total votes: {sumVotes(poll.options)})</p>
         {poll.options.map((item) => (
           <div className="flex flex-col" key={item.id}>
             <p className="text-sm pl-7">{item.option}</p>
