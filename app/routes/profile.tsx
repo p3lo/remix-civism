fde33de90f97be7c7336c10e76781270c73bc830
p3lo <mysiacik@mysiacik.com>
2022-03-31 11:13:51 +0200

    completed logic for tabs

--- app/routes/profile.tsx
+++ app/routes/profile.tsx
@@ -1,15 +1,26 @@
 import { Tabs } from '@mantine/core';
-import { ActionFunction, LoaderFunction, Outlet, redirect, useSubmit } from 'remix';
+import { ActionFunction, json, LoaderFunction, Outlet, redirect, useLoaderData, useSubmit } from 'remix';
 import { FiSettings } from 'react-icons/fi';
 import { BiPoll } from 'react-icons/bi';
 import { auth } from '~/utils/auth.server';
+import { useState } from 'react';
 
 export const loader: LoaderFunction = async ({ request }) => {
   if (!(await auth.isAuthenticated(request))) {
     return redirect('/');
   }
+  const url = new URL(request.url);
 
-  return {};
+  let tabIndex: number;
+  if (url.pathname === '/profile/settings') {
+    tabIndex = 0;
+  } else if (url.pathname === '/profile/mypolls') {
+    tabIndex = 1;
+  } else {
+    return redirect('/profile/settings');
+  }
+
+  return json({ index: tabIndex });
 };
 
 export const action: ActionFunction = async ({ request }) => {
@@ -24,15 +35,18 @@ export const action: ActionFunction = async ({ request }) => {
 };
 
 function ProfileLayout() {
+  const tabInd = useLoaderData();
+  const [activeTab, setActiveTab] = useState(tabInd.index);
   const submit = useSubmit();
 
-  function test(tabindex: number) {
+  function onChange(tabindex: number) {
+    setActiveTab(tabindex);
     const index = tabindex.toString();
     submit({ index }, { method: 'post' });
   }
   return (
     <div className="w-full my-10 mx-auto sm:w-[80%] md:w-[65%] xl:w-[50%]">
-      <Tabs grow onTabChange={test}>
+      <Tabs grow onTabChange={onChange} active={activeTab}>
         <Tabs.Tab label="Settings" icon={<FiSettings size={14} />} />
         <Tabs.Tab label="My polls" icon={<BiPoll size={14} />} />
       </Tabs>
