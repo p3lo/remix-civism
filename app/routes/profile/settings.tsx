07c2b061d2676c51a9c0b624456ecf6474d0423d
p3lo <mysiacik@mysiacik.com>
2022-03-31 22:26:49 +0200

    implemented profile update

--- app/routes/profile/settings.tsx
+++ app/routes/profile/settings.tsx
@@ -1,5 +1,35 @@
+import { Button, TextInput } from '@mantine/core';
+import { ActionFunction, Form, useMatches } from 'remix';
+import { prisma } from '~/db.server';
+
+export const action: ActionFunction = async ({ request }) => {
+  const formData = await request.formData();
+  const name = formData.get('name');
+  const email = formData.get('email');
+  if (name && email) {
+    await prisma.user.update({
+      where: {
+        email: email.toString(),
+      },
+      data: {
+        name: name.toString(),
+      },
+    });
+  }
+  return null;
+};
+
 function SettingsTab() {
-  return <div>SettingsTab</div>;
+  const matches = useMatches()[0].data;
+  return (
+    <Form method="post" className="flex flex-col w-full mx-auto my-10 space-y-3 sm:w-3/4 md:w-2/3 xl:w-1/2">
+      <TextInput placeholder="Your name" label="Name" name="name" required defaultValue={matches.name} />
+      <input hidden value={matches.email} readOnly name="email" />
+      <Button className="w-[200px] mx-auto" variant="subtle" type="submit">
+        Update
+      </Button>
+    </Form>
+  );
 }
 
 export default SettingsTab;
